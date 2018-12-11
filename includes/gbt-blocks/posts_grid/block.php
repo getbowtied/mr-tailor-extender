da1563e0b6b57e61876df3fdf93fb1340669c9bc
Vanesa Reinerth <vanesa@getbowtied.com>
2018-12-11 10:56:12 +0200

    ...

--- includes/gbt-blocks/posts_grid/block.php
+++ includes/gbt-blocks/posts_grid/block.php
@@ -64,50 +64,53 @@ if ( ! function_exists( 'gbt_18_mt_render_frontend_posts_grid' ) ) {
 	        <div class="gbt_18_mt_posts_grid <?php echo $className; ?>">
 	    
 	    		<div class="gbt_18_mt_posts_grid_wrapper <?php echo $className == 'is-style-default' ? 'columns-'.$columns : ''; ?> <?php echo $align; ?>">
-		                    
-		            <?php foreach($recentPosts as $post) : ?>
-		        
-		                <?php $post_format = get_post_format($post->ID); ?>
+		            
+		            <div class="gbt_18_mt_posts_grid_items">
 
-		                <div class="gbt_18_mt_posts_grid_item <?php echo $post_format ? $post_format: 'standard'; ?> <?php if ( !has_post_thumbnail($post->ID)) : ?>no_thumb<?php endif; ?>">
-		                    
-							<a class="gbt_18_mt_posts_grid_item_link" href="<?php echo get_post_permalink($post->ID); ?>">
-								<span class="gbt_18_mt_posts_grid_img_container">
-									<span class="gbt_18_mt_posts_grid_img_overlay"></span>
-									
-									<?php if ( has_post_thumbnail($post->ID)) :
-										$image_id = get_post_thumbnail_id($post->ID);
-										$image_url = wp_get_attachment_image_src($image_id,'large', true);
-									?>
-										<span class="gbt_18_mt_posts_grid_img gbt_18_mt_posts_grid_with_img" style="background-image: url(<?php echo esc_url($image_url[0]); ?> );"></span>
-									<?php else : ?>
-										<span class="gbt_18_mt_posts_grid_img gbt_18_mt_posts_grid_noimg"></span>
-									<?php endif;  ?>
+			            <?php foreach($recentPosts as $post) : ?>
+			        
+			                <?php $post_format = get_post_format($post->ID); ?>
 
-								</span>
+			                <div class="gbt_18_mt_posts_grid_item <?php echo $post_format ? $post_format: 'standard'; ?> <?php if ( !has_post_thumbnail($post->ID)) : ?>no_thumb<?php endif; ?>">
+			                    
+								<a class="gbt_18_mt_posts_grid_item_link" href="<?php echo get_post_permalink($post->ID); ?>">
+									<span class="gbt_18_mt_posts_grid_img_container">
+										<span class="gbt_18_mt_posts_grid_img_overlay"></span>
+										
+										<?php if ( has_post_thumbnail($post->ID)) :
+											$image_id = get_post_thumbnail_id($post->ID);
+											$image_url = wp_get_attachment_image_src($image_id,'large', true);
+										?>
+											<span class="gbt_18_mt_posts_grid_img gbt_18_mt_posts_grid_with_img" style="background-image: url(<?php echo esc_url($image_url[0]); ?> );"></span>
+										<?php else : ?>
+											<span class="gbt_18_mt_posts_grid_img gbt_18_mt_posts_grid_noimg"></span>
+										<?php endif;  ?>
 
-								<?php if($className == 'is-style-default') : ?>
-									<a class="gbt_18_mt_posts_grid_title" href="<?php echo get_post_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>
-								<?php elseif($className == 'is-style-list') : ?>
-									<span class="gbt_18_mt_posts_grid_content_wrapper">
-										<span class="gbt_18_mt_posts_grid_content_inner">
-											
-											<span class="gbt_18_mt_posts_grid_item_day"><?php echo date('d', strtotime($post->post_date)); ?></span>
-											
-											<span class="gbt_18_mt_posts_grid_content">
-												<span class="gbt_18_mt_posts_grid_item_date"><?php echo date('F Y', strtotime($post->post_date)); ?></span>
-												<h3 class="gbt_18_mt_posts_grid_item_title"><?php echo $post->post_title; ?></h3>
-											</span>
-											
-										</span>
 									</span>
-								<?php endif; ?>
-							</a>
-		                    
-		                </div>
-		    
-		            <?php endforeach; ?>
 
+									<?php if($className == 'is-style-default') : ?>
+										<a class="gbt_18_mt_posts_grid_title" href="<?php echo get_post_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>
+									<?php elseif($className == 'is-style-list') : ?>
+										<span class="gbt_18_mt_posts_grid_content_wrapper">
+											<span class="gbt_18_mt_posts_grid_content_inner">
+												
+												<span class="gbt_18_mt_posts_grid_item_day"><?php echo date('d', strtotime($post->post_date)); ?></span>
+												
+												<span class="gbt_18_mt_posts_grid_content">
+													<span class="gbt_18_mt_posts_grid_item_date"><?php echo date('F Y', strtotime($post->post_date)); ?></span>
+													<h3 class="gbt_18_mt_posts_grid_item_title"><?php echo $post->post_title; ?></h3>
+												</span>
+												
+											</span>
+										</span>
+									<?php endif; ?>
+								</a>
+			                    
+			                </div>
+			    
+			            <?php endforeach; ?>
+
+			        </div>
 				</div>
 			</div>
 
