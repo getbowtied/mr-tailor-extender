5bdbea7931dc2a6bdd31887e6293beffdd75360a
Vanesa Reinerth <vanesa@getbowtied.com>
2019-01-31 13:07:54 +0200

    social media profiles block

--- includes/gbt-blocks/social_media_profiles/block.php
+++ includes/gbt-blocks/social_media_profiles/block.php
@@ -24,21 +24,6 @@ if ( ! function_exists( 'gbt_18_mt_social_media_editor_assets' ) ) {
 }
 
 //==============================================================================
-//  Enqueue Frontend Assets
-//==============================================================================
-add_action( 'enqueue_block_assets', 'gbt_18_mt_social_media_assets' );
-if ( ! function_exists( 'gbt_18_mt_social_media_assets' ) ) {
-    function gbt_18_mt_social_media_assets() {
-        
-        wp_enqueue_style(
-            'gbt_18_mt_social_media_styles',
-            plugins_url( 'assets/css/style.css', __FILE__ ),
-            array()
-        );
-    }
-}
-
-//==============================================================================
 //  Register Block Type
 //==============================================================================
 if ( function_exists( 'register_block_type' ) ) {
