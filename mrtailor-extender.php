7d614c62a0ee8c2aceac39349e72628db7c09fd4
Vanesa Reinerth <vanesa@getbowtied.com>
2019-02-22 11:13:58 +0200

    v.1.2.1

--- mrtailor-extender.php
+++ mrtailor-extender.php
@@ -4,11 +4,11 @@
  * Plugin Name:       		Mr. Tailor Extender
  * Plugin URI:        		https://mrtailor.wp-theme.design/
  * Description:       		Extends the functionality of Mr. Tailor with Gutenberg elements.
- * Version:           		1.2
+ * Version:           		1.2.1
  * Author:            		GetBowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		5.0
- * Tested up to: 			5.0.3
+ * Tested up to: 			5.1
  *
  * @package  Mr. Tailor Extender
  * @author   GetBowtied
