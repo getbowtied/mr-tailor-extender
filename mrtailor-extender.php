227297486a0fbe2c9851642d9d0a44561f3d3eba
Vanesa Reinerth <vanesa@getbowtied.com>
2019-08-20 11:15:26 +0300

    v.1.3.4

--- mrtailor-extender.php
+++ mrtailor-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Mr. Tailor Extender
  * Plugin URI:        		https://mrtailor.wp-theme.design/
  * Description:       		Extends the functionality of Mr. Tailor with theme specific features.
- * Version:           		1.3.3
+ * Version:           		1.3.4
  * Author:            		GetBowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		5.0
