7642fba1114b1ef81931b54c790b383c2f8a21cf
Vanesa Reinerth <vanesa@getbowtied.com>
2020-06-18 18:09:17 +0300

    ...

--- includes/custom-menu/assets/js/custom-menu.js
+++ includes/custom-menu/assets/js/custom-menu.js
@@ -19,8 +19,8 @@ jQuery(function($) {
         }
     });
 
-    $('.main-navigation > ul > li.menu-item-megamenu .sub-menu ul.menu-item-inner-submenu').css( 'max-height', $(window).height() - $('.main-navigation > ul > li.menu-item-megamenu > .sub-menu').offset().top - 100 );
     $('.main-navigation > ul > li.menu-item-megamenu .sub-menu li.menu-item-inner-wrapper').each(function() {
+        $(this).find('ul.menu-item-inner-submenu').css( 'max-height', $(window).height() - $('.main-navigation > ul > li.menu-item-megamenu > .sub-menu').offset().top - 100 );
         $(this).css( 'min-height', $(this).find('ul.menu-item-inner-submenu').outerHeight() );
     });
 
