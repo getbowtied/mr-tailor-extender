035fc3ba1a9d92679bf06b7bccd48f5c0a4283be
Romi H. <romi@getbowtied.com>
2025-11-11 11:21:48 +0200

    4.0

--- mr-tailor-extender.php
+++ mr-tailor-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Mr. Tailor Extender
  * Plugin URI:        		https://mrtailor.getbowtied.com
  * Description:       		Extends the functionality of Mr. Tailor with theme specific features.
- * Version:           		3.9
+ * Version:           		4.0
  * Author:            		Get Bowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		6.0
