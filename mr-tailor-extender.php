f2ea20d8644b76a188a3f7b5cda4d971c211997a
Romi H. <romi@getbowtied.com>
2026-08-28 12:32:33 +0300

    theme updater

--- mr-tailor-extender.php
+++ mr-tailor-extender.php
@@ -43,6 +43,8 @@ if ( ! class_exists( 'MrTailorExtender' ) ) :
 				return;
 			}
 
+			require_once dirname( __FILE__ ) . '/core/theme-updater/class-gbt-extender-theme-updater.php';
+
 			if ( ! function_exists( 'is_plugin_active' ) ) {
 				require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 			}
